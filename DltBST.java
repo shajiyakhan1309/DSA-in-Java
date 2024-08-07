36afea1d598f91d96c0c43dafd89ae826e4f73fc
shajiyakhan1309 <shajiyakhan1309@gmail.com>
2024-08-07 16:08:22 +0530

    Binary Search

---
+++ DltBST.java
@@ -0,0 +1,94 @@
+public class DltBST{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node (int data){
+            this.data = data;
+        }
+    }
+    public static Node insert(Node root,int val){
+        if(root==null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.data>val){
+            // left subtree
+            root.left = insert(root.left,val);
+        }
+        else{
+            root.right = insert(root.right,val);
+        }
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
+    public static boolean search(Node root,int key){
+        if(root == null){
+            return false;
+        }
+        if(root.data>key){
+            return search(root.left, key);
+        }
+        else if(root.data ==key){
+            return true;
+        }
+        else{
+            return search(root.right, key);
+        }
+    }
+
+    public static Node delete(Node root,int val){
+        if(root.data > val){
+            root.left = delete(root.left, val);
+        }
+        else if(root.data<val){
+            root.right = delete(root.right, val);
+        }
+        else{
+            // root.data == val
+            // case 1
+            if(root.left == null && root.right==null){
+                return null;
+            }
+            // case 2
+            if(root.left==null){
+                return root.right;
+            }
+            else if(root.right==null){
+                return root.left;
+            }
+            // case 3
+            Node Is = inorderSuccessor(root.right);
+            root.data = Is.data;
+            root.right = delete(root.right, Is.data);
+        }
+        return root;
+    }
+    public static Node inorderSuccessor(Node root){
+        while (root.left!=null) {
+            root = root.left;
+        }
+        return root;
+    }
+    public static void main(String args[]){
+        int values[]={8,5,3,1,4,6,10,11,14};
+        Node root = null;
+        for(int i=0;i<values.length;i++){
+            root=insert(root,values[i]);
+        }
+        inorder(root);
+        System.out.println();
+        
+        delete(root, 4);
+        inorder(root);
+    }
+}
---
+++ RToLBST.java
@@ -0,0 +1,138 @@
+import java.util.ArrayList;
+
+public class RToLBST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data > key) {
+            return search(root.left, key);
+        } else if (root.data == key) {
+            return true;
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+    public static Node delete(Node root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else {
+            // root.data == val
+            // case 1: no children
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // case 2: one child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            // case 3: two children
+            Node successor = inorderSuccessor(root.right);
+            root.data = successor.data;
+            root.right = delete(root.right, successor.data);
+        }
+        return root;
+    }
+
+    public static Node inorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static void printInRange(Node root, int x, int y) {
+        if (root == null) {
+            return;
+        }
+        if (root.data >= x && root.data <= y) {
+            printInRange(root.left, x, y);
+            System.out.println(root.data + " ");
+            printInRange(root.right, x, y);
+        } else if (root.data > y) {
+            printInRange(root.left, x, y);
+        } else {
+            printInRange(root.right, x, y);
+        }
+    }
+
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            if (i == path.size() - 1) {
+                System.out.print(path.get(i));
+            } else {
+                System.out.print(path.get(i) + "->");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.data);
+        // leaf
+        if (root.left == null && root.right == null) {
+            printPath(path);
+        } else {
+            // non-leaf
+            printRoot2Leaf(root.left, path);
+            printRoot2Leaf(root.right, path);
+        }
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+        
+        printRoot2Leaf(root, new ArrayList<>());
+    }
+}
